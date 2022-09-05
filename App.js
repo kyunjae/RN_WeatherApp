9043d643f9d0cf88877401c5c5cc0deb11960244
강윤재 <gang-yunjae@gang-yunjaeui-MacBookPro.local>
2022-09-05 20:05:44 +0900

    프로젝트 생성

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <View>
+          <Text>Hi! I'm going to make a RN weather App!</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
