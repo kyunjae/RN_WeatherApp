479e120543925d09d008b598c94fc49781fda43d
강윤재 <gang-yunjae@gang-yunjaeui-MacBookPro.local>
2022-09-06 20:48:46 +0900

    app design 완료

--- App.js
+++ App.js
@@ -2,31 +2,82 @@ import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
+  ScrollView,
   StyleSheet,
+  Dimensions,
   Text,
   View,
 } from 'react-native';
 
+const { width:SCREEN_WIDTH } = Dimensions.get("window");
+
+console.log(SCREEN_WIDTH)
+
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-        <View style={{flex: 2, backgroundColor: "tomato"}}>
-          <Text>Hi! I'm going to make a RN App!</Text>
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
         </View>
-        <View style={{flex: 3, backgroundColor: "teal"}}>
-
-        </View>
-        <View style={{flex: 1, backgroundColor: "orange"}}>
-
-        </View>
-    </SafeAreaView>
+        <ScrollView 
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        //indicatorStyle="white"
+        contentContainerStyle={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View><View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </ScrollView>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    backgroundColor: "teal",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather:{
+    
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontWeight: "500",
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
 
